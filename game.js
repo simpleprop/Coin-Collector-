e10a523fb2f13cfec0f00b7d1b87e5f847eb90f4
simpleprop <nikgurov06@gmail.com>
2021-07-22 22:56:21 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-alert('10');
+alert('11');
 // define variables
 var game;
 var player;
@@ -22,15 +22,15 @@ function addItems() {
 // add platforms to the game
 function addPlatforms() {
   platforms = game.add.physicsGroup();
-  platforms.create(480, 360, 'platform');
-  platforms.create(380, 500, 'platform');
+  platforms.create(580, 360, 'platform');
+  platforms.create(300, 500, 'platform');
   platforms.create(200, 460, 'platform');
-  platforms.create(300, 750, 'platform2');
-  platforms.create(400, 560, 'platform2');
-  platforms.create(600, 600, 'platform2');
+  platforms.create(520, 750, 'platform2');
+  platforms.create(200, 560, 'platform2');
+  platforms.create(300, 600, 'platform2');
   platforms.create(570, 300, 'platform');
-  platforms.create(280, 290, 'platform');
-  platforms.create(240, 240, 'platform');
+  platforms.create(280, 490, 'platform');
+  platforms.create(480, 240, 'platform');
   platforms.create(640, 100, 'platform');
   platforms.create(110, 120, 'platform2');
   platforms.setAll('body.immovable', true);
