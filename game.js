05e369436441d6c7622bfdcd0498eaf3117687df
simpleprop <nikgurov06@gmail.com>
2021-08-04 22:47:14 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-alert('Version - 13.5.7 Click "OK" to continue');
+alert('Version - 13.5.8 Click "OK" to continue');
 // define variables
 var game;
 var player;
@@ -17,7 +17,7 @@ var winningScore = 1000000000000000000000000000000000000000000000000000000000000
 function addItems() {
   items = game.add.physicsGroup();
   createItem(120, 50,  'star');
-  createItem(393, 350, 'coin');
+  createItem(390, 370, 'coin');
 }
 
 // add platforms to the game
