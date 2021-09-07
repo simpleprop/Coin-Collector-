60d255a27e4f79d02bb0f6335cdca8325b14583b
simpleprop <nikgurov06@gmail.com>
2021-09-07 17:07:59 +0300

    b

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-alert('Version - 13.6.22 Click "OK" to continue');
-alert('Winning score is 110. Dont touch the poison!');
+alert('Version - 13.6.23 Click "OK" to continue');
+alert('Winning score is 110. Collect all coins and dont touch the poison');
 // define variables
 var game;
 var player;
@@ -26,7 +26,7 @@ function addItems() {
   createItem(250, 600, 'poison');
   createItem(450, 140, 'coin');
 
-  createItem(480, 700, 'coin');
+
   createItem(260, 560, 'coin');
   createItem(150, 560, 'coin');
 }
